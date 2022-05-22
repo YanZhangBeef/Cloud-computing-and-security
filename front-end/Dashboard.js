415480b73ac810ebbffe1d055b882fc27f2be0e6
Yan ZHANG <yanz21@student.unimelb.edu.au>
2022-05-23 00:08:56 +0800

    add upload

--- front-end/Dashboard.js
+++
@@ -1,7 +0,0 @@
-const imageForm = document.querySelector("#imageForm")
-const imageInput = document.querySelector("#imageInput")
-
-imageForm.addEventListener("submit", async event => {
-    event.preventDefault()
-    const file=imageInput.files[0]
-})
